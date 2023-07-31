5d339ed5d4d802bb0be79b387b229be7d5a06c8f
blackredgate <138416861+blackredgate@users.noreply.github.com>
2023-07-31 08:52:14 +0000

    [Add]主要なviewページ、新規投稿機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  
   def index
     @user = current_user
     @books = Book.all
@@ -6,8 +7,27 @@ class BooksController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to user_path(current_user.id)
+  end
+  
+  def destroy
+    
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @book = Book.new
   end
 
   def edit
@@ -9,6 +10,8 @@ class UsersController < ApplicationController
   end
   
   def index
+    @user = current_user
     @users = User.all
+    @book = Book.new
   end
 end
