49b530ef08df5f11590205c2f152fab0027a2c42
blackredgate <138416861+blackredgate@users.noreply.github.com>
2023-08-01 03:43:44 +0000

    [Add]book/userでのcreate/destroyアクション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,8 +7,8 @@ class BooksController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
     @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def edit
@@ -22,8 +22,16 @@ class BooksController < ApplicationController
     redirect_to user_path(current_user.id)
   end
   
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
+  end
+  
   def destroy
-    
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get 'homes/about' => 'homes#about', as: 'about'
   resources :books
-  resources :users, only: [:edit, :index, :show]
+  resources :users, only: [:edit, :index, :show, :update]
 end
